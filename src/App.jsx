1d7dc6c350678c7490408546d85e84f0404a8c19
AkashItlogiko <akashsahanwu089@gmail.com>
2024-10-27 19:43:01 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,47 @@ import { useState } from 'react';
 
 const App = () => {
   const [list, setList] = useState([]);
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState(['']);
+
+  const AddToList = () => {
+    list.push(item);
+    setList([...list]);
+  };
+
+  const RemoveItem = index => {
+    list.splice(index, 1);
+    setList([...list]);
+  };
 
   return (
     <div>
-      <input placeholder="Item" />
-      <button>Add</button>
+      <table>
+        <tbody>
+          {list.length != 0 ? (
+            list.map((element, index) => {
+              return (
+                <tr>
+                  <td>{element}</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        RemoveItem(index);
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr></tr>
+          )}
+        </tbody>
+      </table>
+
+      <input onChange={e => setItem(e.target.value)} placeholder="Item" />
+      <button onClick={AddToList}>Add</button>
     </div>
   );
 };
